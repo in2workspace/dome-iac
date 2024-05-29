ecad54df2741c2cf5cab94551a862bdb6d9fb5ce
Oriol Canadés <oriol.canades@in2.es>
2024-05-29 09:09:06 +0200

    Update DOME-SAAS

--- dome-saas/scripts/issuer-keycloak-init.sh
+++ dome-saas/scripts/issuer-keycloak-init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm cert_*
 rm truststore.jks
 
@@ -8,20 +10,22 @@ count=1
 for certificate in $certificates; do
 	file="cert_$count.pem"
 	if [ -s "$file" ]; then
-    		echo "$file ya existe."
+    		echo "$file exists."
 	else
     		echo "-----BEGIN CERTIFICATE-----
-$certificate
------END CERTIFICATE-----" > "$file"
+              $certificate
+              -----END CERTIFICATE-----" > "$file"
 	fi
-	# Generar file .der para importar en el truststore
+	# Generate .der file which will be imported into the truststore
 	openssl x509 -in "$file" -out "cert_$count.der" -outform DER
-	# Importar el certificate al trustore
-	yes si | /mnt/c/Users/LauraGarciaFernandez/.jdks/corretto-11.0.22/bin/keytool.exe -import -alias "cert_$count" -file "cert_$count.der" -keystore truststore.jks -storepass pempas
+	# Import the certificate into truststore
+	yes si | keytool -import -alias "cert_$count" -file "cert_$count.der" -keystore /opt/keycloak/conf/truststore.jks -storepass pempas
 	count=$((count + 1))
 done
 
-cp truststore.jks ../keycloak2/truststore.jks
 rm cert_*
 
-echo "FIN"
+echo "END"
+
+# Iniciar Keycloak
+/opt/keycloak/bin/kc.sh start-dev
